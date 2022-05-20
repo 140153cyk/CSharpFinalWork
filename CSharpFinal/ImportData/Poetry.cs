c95b9d70d76f3f717c9568b166831b0170bee27d
140153cyk <99869808+140153cyk@users.noreply.github.com>
2022-05-20 09:44:51 +0800

    需求和设计文档

---
+++ CSharpFinal/ImportData/Poetry.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImportData
+{
+   public class Poetry
+    {
+        public string title { get; set; }
+        public string author { get; set; }
+        public List<string> paragraphs { get; set; }
+
+
+
+    }
+}
