b1eb18e8f203a004627b191f64ad511911901dc3
zl <1276521436@qq.com>
2022-05-25 13:45:57 +0800

    11

--- CSharpFinal/Basic/Robot.Designer.cs
+++ CSharpFinal/Basic/Robot.Designer.cs
@@ -28,12 +28,88 @@
     /// </summary>
     private void InitializeComponent()
     {
-      this.components = new System.ComponentModel.Container();
-      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-      this.ClientSize = new System.Drawing.Size(800, 450);
-      this.Text = "Robot";
+            this.uiLabel1 = new Sunny.UI.UILabel();
+            this.uiButton1 = new Sunny.UI.UIButton();
+            this.uiTextBox1 = new Sunny.UI.UITextBox();
+            this.uiTextBox2 = new Sunny.UI.UITextBox();
+            this.SuspendLayout();
+            // 
+            // uiLabel1
+            // 
+            this.uiLabel1.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.uiLabel1.Location = new System.Drawing.Point(71, 53);
+            this.uiLabel1.Name = "uiLabel1";
+            this.uiLabel1.Size = new System.Drawing.Size(100, 23);
+            this.uiLabel1.TabIndex = 0;
+            this.uiLabel1.Text = "关键词";
+            this.uiLabel1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.uiLabel1.ZoomScaleRect = new System.Drawing.Rectangle(0, 0, 0, 0);
+            // 
+            // uiButton1
+            // 
+            this.uiButton1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.uiButton1.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.uiButton1.Location = new System.Drawing.Point(561, 53);
+            this.uiButton1.MinimumSize = new System.Drawing.Size(1, 1);
+            this.uiButton1.Name = "uiButton1";
+            this.uiButton1.Size = new System.Drawing.Size(100, 35);
+            this.uiButton1.TabIndex = 1;
+            this.uiButton1.Text = "作诗";
+            this.uiButton1.TipsFont = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.uiButton1.ZoomScaleRect = new System.Drawing.Rectangle(0, 0, 0, 0);
+            this.uiButton1.Click += new System.EventHandler(this.uiButton1_Click);
+            // 
+            // uiTextBox1
+            // 
+            this.uiTextBox1.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.uiTextBox1.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.uiTextBox1.Location = new System.Drawing.Point(288, 53);
+            this.uiTextBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.uiTextBox1.MinimumSize = new System.Drawing.Size(1, 16);
+            this.uiTextBox1.Name = "uiTextBox1";
+            this.uiTextBox1.ShowText = false;
+            this.uiTextBox1.Size = new System.Drawing.Size(150, 29);
+            this.uiTextBox1.TabIndex = 2;
+            this.uiTextBox1.TextAlignment = System.Drawing.ContentAlignment.MiddleLeft;
+            this.uiTextBox1.ZoomScaleRect = new System.Drawing.Rectangle(0, 0, 0, 0);
+            this.uiTextBox1.TextChanged += new System.EventHandler(this.uiTextBox1_TextChanged);
+            // 
+            // uiTextBox2
+            // 
+            this.uiTextBox2.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.uiTextBox2.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.uiTextBox2.Location = new System.Drawing.Point(220, 171);
+            this.uiTextBox2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.uiTextBox2.MinimumSize = new System.Drawing.Size(1, 16);
+            this.uiTextBox2.Name = "uiTextBox2";
+            this.uiTextBox2.ReadOnly = true;
+            this.uiTextBox2.ShowText = false;
+            this.uiTextBox2.Size = new System.Drawing.Size(329, 60);
+            this.uiTextBox2.TabIndex = 3;
+            this.uiTextBox2.TextAlignment = System.Drawing.ContentAlignment.MiddleLeft;
+            this.uiTextBox2.ZoomScaleRect = new System.Drawing.Rectangle(0, 0, 0, 0);
+            this.uiTextBox2.TextChanged += new System.EventHandler(this.uiTextBox2_TextChanged);
+            // 
+            // Robot
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.uiTextBox2);
+            this.Controls.Add(this.uiTextBox1);
+            this.Controls.Add(this.uiButton1);
+            this.Controls.Add(this.uiLabel1);
+            this.Name = "Robot";
+            this.Text = "Robot";
+            this.ResumeLayout(false);
+
     }
 
     #endregion
+
+    private Sunny.UI.UILabel uiLabel1;
+    private Sunny.UI.UIButton uiButton1;
+    private Sunny.UI.UITextBox uiTextBox1;
+    private Sunny.UI.UITextBox uiTextBox2;
   }
 }
--- CSharpFinal/Basic/Robot.cs
+++ CSharpFinal/Basic/Robot.cs
@@ -1,20 +1,75 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web.Script.Serialization;
 using System.Windows.Forms;
 
 namespace Basic
 {
-    public partial class Robot: Form
+    
+    public partial class Robot : Form
     {
-        public Robot()
-        {
-            InitializeComponent();
-        }
+      public class Newslists
+      {
+        public string List { get; set; }
+      }
+      public class Content
+      {
+        public int Code { get; set; }
+        public string Msg { get; set; }
+        public List<Newslists> Newslist { get; set; }
+      }
+      public Robot()
+      {
+        InitializeComponent();
+        //this.BackgroundImage = Image.FromFile("D:\\loginView\\500.jpg");
+        uiTextBox2.Multiline = true;
+        uiTextBox2.AutoSize = false;
+        uiTextBox2.Height = 200;
+      }
+    public static string HttpGet(string word)
+    {
+      //ServicePointManager.ServerCertificateValidationCallback = new RemoteCertificateValidationCallback(CheckValidationResult);
+      Encoding encoding = Encoding.UTF8;
+      HttpWebRequest request = (HttpWebRequest)WebRequest.Create("http://api.tianapi.com/cangtoushi/index?key=c4eba90cfbb5608049513494bba1eb22&word=" + word);
+      request.Method = "GET";
+      request.Accept = "text/html, application/xhtml+xml, */*";
+      request.ContentType = "application/json";
+
+      HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+      using (StreamReader reader = new StreamReader(response.GetResponseStream(), Encoding.UTF8))
+      {
+        return reader.ReadToEnd();
+      }
     }
+    private void uiTextBox2_TextChanged(object sender, EventArgs e)
+    {
+    }
+
+    private void uiTextBox1_TextChanged(object sender, EventArgs e)
+    {
+
+    }
+
+    private void uiButton1_Click(object sender, EventArgs e)
+    {
+      var ss = HttpGet(uiTextBox1.Text);
+            JavaScriptSerializer js = new JavaScriptSerializer();
+            var model = js.Deserialize<Content>(ss);
+            foreach (var temp in model.Newslist)
+            {
+               
+                //MessageBox.Show(temp.List);
+                uiTextBox2.AppendText (temp.List+ Environment.NewLine);
+                
+            }
+    }
+  }
 }
