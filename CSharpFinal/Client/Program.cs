0b2ba2bc498656fc95327da72cb32c837f6ac064
404known <87415489+404known@users.noreply.github.com>
2022-06-09 19:29:09 +0800

    Add files via upload

---
+++ CSharpFinal/Client/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+namespace Client
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("a");
+        }
+    }
+}
